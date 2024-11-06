a5f9d18be03fde64ded20a4e2f38ea5e12e4f074
tagty <bouzu.ao@gmail.com>
2024-11-06 09:23:04 +0900

    Finish APIでBillingReportを生成する #19

--- webapp/sql/init.sh
+++ webapp/sql/init.sh
@@ -20,7 +20,7 @@ mysql -u"$ISUCON_DB_USER" \
 		-p"$ISUCON_DB_PASSWORD" \
 		--host "$ISUCON_DB_HOST" \
 		--port "$ISUCON_DB_PORT" \
-		"$ISUCON_DB_NAME" < admin/11_create_billing_report.sql
+		"$ISUCON_DB_NAME" < admin/billing_report.sql
 
 # SQLiteのデータベースを初期化
 rm -f ../tenant_db/*.db
